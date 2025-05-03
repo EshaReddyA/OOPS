61047bbb096f3f5431d13050450fa3a75af558fd
Esha Reddy Annapureddy <eshasplej5@gmail.com>
2025-05-02 21:55:34 -0500

    question

---
+++ Decorators/2.py
@@ -0,0 +1,15 @@
+class Decorators:
+    @staticmethod
+    def start_end(func):
+        def wrapper():
+            print("Start")
+            func()
+            print("End")
+        return wrapper
+
+class Greeter:
+    @Decorators.start_end
+    def greet():
+        print("Hello!")
+
+Greeter.greet()
